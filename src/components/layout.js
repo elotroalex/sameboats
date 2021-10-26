8f291de17f8b388ba143c02f04d068542563d77d
Alex Gil <colibri.alex@gmail.com>
2021-10-26 15:47:28 -0400

    removed footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -15,15 +15,6 @@ const Layout = ({ children }) => {
     <>
       <Navbar />
       <main>{children}</main>
-      <footer
-        style={{
-          marginTop: `2rem`,
-        }}
-      >
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.com">Gatsby</a>
-      </footer>
     </>
   );
 };
