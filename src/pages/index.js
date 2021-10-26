203016eec917294a895a9eb35188b7a3473694dd
Alex Gil <colibri.alex@gmail.com>
2021-10-26 11:07:59 -0400

    Added text to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,69 @@
-import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
+import * as React from "react";
+import { Link } from "gatsby";
+import { StaticImage } from "gatsby-plugin-image";
 
-import Layout from "../components/layout"
-import Seo from "../components/seo"
+import Layout from "../components/layout";
+import Seo from "../components/seo";
 
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
+    <h1>Welcome</h1>
+    <p>
+      In the Same Boats is a work of multimodal scholarship designed to
+      encourage the collaborative production of humanistic knowledge within
+      scholarly communities. The platform comprises two interactive
+      visualizations that trace the movements of significant cultural actors
+      from the Caribbean and wider Americas, Africa, and Europe within the 20th
+      century Afro-Atlantic world. It presents opportunities for unearthing the
+      extent to which Caribbean, Latin American, African, European, and
+      Afro-American intellectuals have been in both punctual and sustained
+      conversation with one another: attending the same conferences, publishing
+      in the same journals and presses, active in the same political groups,
+      perhaps even elbow-to-elbow in the same Parisian cafés and on the same
+      transatlantic crossings––literally and metaphorically in the same
+      boats––as they circulate throughout the Americas, Africa, Europe, and
+      beyond.
+    </p>
+    <p>
+      One of the greatest challenges for postcolonial scholars of Africa and its
+      diasporas is that of navigating nation-language frontiers that are the
+      legacies of colonialism. The partitioning of the postcolonial into
+      anglophone, francophone, hispanophone, and lusophone spaces
+      persists—despite being in so many ways at odds with the shared
+      socio-cultural and historical realities of the Afro-Atlantic. Same Boats
+      pushes back against the academic balkanization of black Atlantic
+      intellectual production, of retracing limits and contours largely
+      determined by imperial metropoles.
+    </p>
+    <p>
+      In presenting a series of individual maps produced on a common template by
+      specialists of the different linguistic regions of the western Atlantic,
+      Same Boats seeks to counter the Euro-lingual constraints and attendant
+      border-drawing that too often keep Caribbeanist, Latin-Americanist,
+      Africanist, Afro-Europeanist, and Afro-Americanist scholars from engaging
+      in deeply transnational and transcolonial dialogue, hindering us from
+      seeing connections or from fully imagining a networked Afro-intellectual
+      world. Literally mapping the network of Afro-diasporic intellectual,
+      political, and aesthetic figures in the 20th century, Same Boats offers
+      visual corroboration of the work of scholars who have long committed to
+      thinking about the Afro-Atlantic as an integrally networked chronotope.
+      This platform aims to facilitate a physical tracing of hemispheric black
+      studies – to provide a point of departure from which to highlight the
+      various sites wherein Afro-Atlantic intellectual traditions may have been
+      born and/or shepherded into the world.
+    </p>
+    <p>
+      The platform’s visualizations sketch a “big picture” narrative of Black
+      intellectual and artistic migration, revealing what were the potential
+      points and periods of encounter over the course of the twentieth century.
+      Upon drilling down into the maps’ spatio-temporal intersections, specific
+      nodes of confluence emerge, calling – we hope – for further scholarly
+      exploration. Indeed, Same Boats is meant to serve above all as an
+      invitation for our community of scholars to flesh out and enrich the
+      scholarly record toward the constitution of an Afro-Atlantic Republic of
+      Arts and Letters.
+    </p>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
@@ -26,6 +71,6 @@ const IndexPage = () => (
       <Link to="/using-dsg">Go to "Using DSG"</Link>
     </p>
   </Layout>
-)
+);
 
-export default IndexPage
+export default IndexPage;
