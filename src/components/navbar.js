d4765d568e249d0dd90f52b2775c5f4d7609cd0d
Alex Gil <colibri.alex@gmail.com>
2021-10-31 23:28:54 -0400

    added styled components to header

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,38 +1,53 @@
 import React from "react";
 import { Link } from "gatsby";
-import PropTypes from "prop-types";
 import "./navbar.css";
-import itsbIcon from "../images/itsb_icon.svg";
 import { menuData } from "../data/menuData";
+import styled from "styled-components";
+import logo from "../images/itsb_icon.svg";
 
-const Header = ({ siteTitle }) => (
-  <div className="title">
-    <Link to="/">
-      <div>
-        <img
-          className="title-icon"
-          src={itsbIcon}
-          alt="Same Boats Logo: A concentric circle."
-        />
-        <span className="title-text">{siteTitle}</span>
-      </div>
-    </Link>
-    <script></script>
-  </div>
+// HEADER
+
+const Title = styled.div`
+  height: 4rem;
+  background: #fcb040;
+`;
+
+const TitleLink = styled(Link)`
+  width: 100%;
+  text-decoration: none;
+  color: black;
+  font-size: 1.2rem;
+  font-weight: 600;
+  display: flex;
+  flex-flow: row nowrap;
+`;
+
+const TitleText = styled.span`
+  text-transform: uppercase;
+  padding-top: 1.4rem;
+`;
+
+const TitleIcon = styled.img`
+  padding: 1rem;
+  height: 30px;
+  width: 2rem;
+`;
+
+const Header = () => (
+  <Title>
+    <TitleLink to="/">
+      <TitleIcon src={logo} alt="Same Boats Logo" />
+      <TitleText>In The Same Boat</TitleText>
+    </TitleLink>
+  </Title>
 );
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-  siteTitle: "",
-};
+// NAVBAR
 
 export default function Navbar() {
   return (
     <nav className="navbar">
-      <Header siteTitle="In The Same Boat" />
+      <Header />
       <div className="main-menu">
         {menuData.map((link) => (
           <div key={link.name} className="menu-item">
